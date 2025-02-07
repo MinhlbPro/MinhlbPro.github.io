98ae11da2b1ec57bd17fe6dd16c7cd8d170098b8
MinhlbPro <150591061+MinhlbPro@users.noreply.github.com>
2025-02-07 15:30:18 +0900

    Update script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -211,7 +211,7 @@ var Tawk_API = Tawk_API || {},
   var s1 = document.createElement("script"),
     s0 = document.getElementsByTagName("script")[0];
   s1.async = true;
-  s1.src = "https://embed.tawk.to/62e66e1554f06e12d88c29f0/1g9a1s1qo";
+  s1.src = "https://embed.tawk.to/67a5a7e5825083258e116d46/1ijfhfmgr";
   s1.charset = "UTF-8";
   s1.setAttribute("crossorigin", "*");
   s0.parentNode.insertBefore(s1, s0);
