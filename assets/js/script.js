37a92fc787440fb64c09be485116ef55e37bef8d
MinhlbPro <150591061+MinhlbPro@users.noreply.github.com>
2025-02-07 16:30:41 +0900

    Update script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -152,7 +152,7 @@ function showProjects(projects) {
   /* ===== SCROLL REVEAL ANIMATION ===== */
   const srtop = ScrollReveal({
     origin: "top",
-    distance: "180px",
+    distance: "250px",
     duration: 1000,
     reset: true,
   });
