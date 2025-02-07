6f014628d18849d7d25df3afdc3a94e0bcd8a5bb
MinhlbPro <150591061+MinhlbPro@users.noreply.github.com>
2025-02-07 15:54:14 +0900

    Update script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -42,7 +42,7 @@ $(document).ready(function () {
 
   // <!-- emailjs to mail contact form data -->
   $("#contact-form").submit(function (event) {
-    emailjs.init("user_TTDmetQLYgWCLzHTDgqxm");
+    emailjs.init("2g4bQ8IcJwigag6li");
 
     emailjs
       .sendForm("contact_service", "template_contact", "#contact-form")
